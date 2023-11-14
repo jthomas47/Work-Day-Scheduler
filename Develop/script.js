f7cbec36e188382ad0318a5cfa4c001df39fb4dc
jthomas47 <jackspurthomas@gmail.com>
2023-11-14 00:32:19 -0600

    added comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
 var currentDayEl = $('#currentDay'); 
 var hour9El = document.querySelector('#hour-9'); 
 var hour10El = document.querySelector('#hour-10');  
@@ -14,6 +13,8 @@ var hour16El = document.querySelector('#hour-16');
 var hour17El = document.querySelector('#hour-17'); 
 var saveBtn = document.querySelectorAll('.saveBtn'); 
 
+
+//saves items to local storage
 function displaySchedule() {
   
   hour9El.children[1].value = events;
@@ -128,28 +129,7 @@ function handleSave(event) {
   localStorage.setItem(event.target.parentElement.id, text); 
 }
 
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
+//adds event listener to save buttons
 for (let i = 0; i < saveBtn.length; i++) {
   saveBtn[i].addEventListener('click', handleSave); 
 }
